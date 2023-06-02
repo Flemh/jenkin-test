cbe46946decec994e08c3330610952ae8b4732bf
flemh <przemyslaw.niedorozow@gmail.com>
2023-06-02 13:52:00 +0200

    groovy

---
+++ ide/pipeline.groovy
@@ -0,0 +1,32 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                // Get some code from a GitHub repository
+                git url: 'https://github.com/Flemh/jenkin-test.git', branch:'main'
+
+            }
+        }
+        stage('Build') {
+            steps {
+
+                // Run Maven on a Unix agent.
+                sh 'chmod +x mvnw && ./mvnw clean package'
+
+                // To run Maven on a Windows agent, use
+                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
+            }
+
+            post {
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
+                always {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
